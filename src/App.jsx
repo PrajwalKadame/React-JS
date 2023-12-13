683d297571be8935d62c7877d001ac7a9abf624b
PrajwalKadame <prajwalkadame.sangamone@gmail.com>
2023-12-13 10:42:59 +0530

    States

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Nav from './Nav'
 import Refs from './Refs'
+import States from './States'
 
 const App = () => {
   return (
     <div>
-        <Nav a={13}/>
-        <Refs/>
+        {/* <Nav a={13}/> */}
+        {/* <Refs/> */}
+        <States/>
     </div>
   )
 }
---
+++ src/States.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import axiosInstance from './helper/axiosInstance'
+
+const States = () => {
+    let[name, setName]=useState()
+    let[password, setPassword]=useState()
+
+    let submit=(c)=>{
+        c.preventDefault()
+        let payload={name, password}
+
+        axiosInstance.post("posts",payload)
+        alert("Sucess")
+
+    }
+
+  return (
+    <>
+    <form action="">
+        <label htmlFor="name">Name</label>
+        <input type="text" name="name" id="name" 
+        onChange={(a)=>{
+            setName(a.target.value)
+        }}/>
+
+        <label htmlFor="pass">Password</label>
+        <input type="text" name="pss" id="pass" onChange={(b)=>{
+            setPassword(b.target.value)
+        }}/>
+
+        <button type="submit" onClick={submit}>Submit</button>
+        <button type="reset">Cancel</button>
+    </form>
+    </>
+  )
+}
+
+export default States
